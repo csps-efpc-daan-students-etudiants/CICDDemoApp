e034f87034c5247737a67072f07d8d25873be60d
CalvinRodo <calvinv@gmail.com>
2019-03-30 23:30:52 -0400

    First Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form role="form">
+      <TextField text="Username" name="username"/>
+      <TextField text="Password" name="password"/>
+      <button class="btn btn-default" v-on:click="" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TextField from '@/components/TextField.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    TextField,
+  },
+})
+export default class Home extends Vue {}
+</script>
