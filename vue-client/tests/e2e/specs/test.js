6be12653b1cc124bd1aa1b0bef1afab5225d49d9
CalvinRodo <calvinv@gmail.com>
2019-04-12 21:10:20 -0400

    write a test and reset db script

--- vue-client/tests/e2e/specs/test.js
+++ vue-client/tests/e2e/specs/test.js
@@ -1,13 +1,17 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('Login Page', () => {
-  it('Has a ', () => {
-    cy.visit('/')
-    cy.get('#username')
-      .type('bar');
-    cy.get('#password')
-      .type("foo");
-    cy.get('#login')
+describe("Task List", () => {
+  it("adds a task", () => {
+    cy.visit("/");
+    cy.get("#name")
+      .type("do a thing");
+    cy.get("#addTask")
       .click();
-  })
-})
+    cy.get(":nth-child(5) > li > .btn")
+      .contains("do a thing");
+  });
+
+  it("toggles a task", () => {
+    cy.visit("/");
+  });
+});
